243df198929217e760d81d2684557b946ecbb35a
Shams Ali <ali.s90@outlook.com>
2016-03-31 08:01:53 -0600

    added recursive solution for allCombos

--- allCombos/answer.js
+++ allCombos/answer.js
@@ -1,3 +1,5 @@
+//with inner funcstion
+
 var allCombos = function(array){
   var result = [[]];
 
@@ -12,3 +14,22 @@ var allCombos = function(array){
 
   return result;
 };
+
+//no inner funciton
+
+var allCombos = function (array){
+  var res = [];
+  if(array.length===0){
+    res.push([]);
+    return res;
+  }
+  for(var i = 0; i < array.length; i++){
+    res.push([array[i]]);
+    for(var j = i + 1; j < array.length; j++){
+      res[i].push(array[j]);
+    }
+  }
+  return res.concat(allCombos(array.slice(0,-1)));
+};
+
+console.log(allCombos(['a','b','c']));
